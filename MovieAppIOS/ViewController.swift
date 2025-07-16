ca74f947c1145c31441fae36ec93083e45d722d2
sxrgi0 <sergiofh.2002@gmail.com>
2025-07-16 16:15:40 +0200

    Initial Commit

---
+++ MovieAppIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAppIOS
+//
+//  Created by Tardes on 16/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
